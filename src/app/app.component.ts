c11a026e7d89ddbb5de0b606f6d98cab4894057a
josuehoenicka <josuee.vzla@gmail.com>
2024-03-17 16:46:19 -0300

    feat: logic & styles (radio) / template omega v16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RadioComponent } from './radio/radio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RadioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/radio/radio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radio',
+  standalone: true,
+  imports: [],
+  templateUrl: './radio.component.html',
+  styleUrl: './radio.component.css'
+})
+export class RadioComponent {
+
+}
