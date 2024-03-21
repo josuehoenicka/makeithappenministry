f8864c9b94d0e260c06efd8b48b7e1b2928c9b74
josuehoenicka <josuee.vzla@gmail.com>
2024-03-20 22:49:52 -0300

    feat: sidenav styles & logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { PrimeNGConfig } from 'primeng/api';
+import { SidenavComponent } from './common/components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [
+    SidenavComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'makeithappenministry';
-  ripple!: boolean;
-  count = 0;
-  previousUrl!: string;
-  inputStyle = "outlined";
-
-  constructor(private primengConfig: PrimeNGConfig) {}
-
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-    this.ripple = true;
-
-    /* TRADUCE LOS CALENDARS A ESPAÑOL */
-    this.primengConfig.setTranslation({
-      dayNames: [
-        'DOMINGO',
-        'LUNES',
-        'MARTES',
-        'MIÉRCOLES',
-        'JUEVES',
-        'VIERNES',
-        'SÁBADO',
-      ],
-      dayNamesShort: ['DOM', 'LUN', 'MAR', 'MIÉ', 'JUE', 'VIE', 'SÁB'],
-      dayNamesMin: ['DO', 'LU', 'MA', 'MI', 'JU', 'VI', 'SÁ'],
-      monthNames: [
-        'ENERO',
-        'FEBRERO',
-        'MARZO',
-        'ABRIL',
-        'MAYO',
-        'JUNIO',
-        'JULIO',
-        'AGOSTO',
-        'SEPTIEMBRE',
-        'OCTUBRE',
-        'NOVIEMBRE',
-        'DICIEMBRE',
-      ],
-      monthNamesShort: [
-        'ENE',
-        'FEB',
-        'MAR',
-        'ABR',
-        'MAY',
-        'JUN',
-        'JUL',
-        'AGO',
-        'SEP',
-        'OCT',
-        'NOV',
-        'DIC',
-      ],
-      today: 'Hoy',
-      clear: 'Limpiar',
-    });
-  }
+  constructor() {}
 }
---
+++ src/app/common/components/sidenav/sidenav.component.ts
@@ -0,0 +1,76 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-sidenav',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    MatIconModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.css',
+})
+export class SidenavComponent {
+  sidenavHidden: boolean = false;
+  activeRoute: string | null = null;
+  showContent: boolean = false;
+
+  sidenavRoutes = [
+    {
+      title: 'Dashboard',
+    },
+    {
+      title: 'Radio',
+    },
+    {
+      title: 'Proyectos',
+      children: ['Dexcarriados Anonymous', 'Quiz MIH'],
+    },
+    {
+      title: 'Estudios',
+      children: ['Pastores', 'Adoradores', 'Jóvenes'],
+    },
+    {
+      title: 'Equipo',
+      children: [
+        'Fundadores',
+        'Pastores',
+        'Maestros',
+        'Radio',
+        'Marketing',
+        'Soporte Técnico',
+      ],
+    },
+    {
+      title: 'Más información',
+      children: ['¿Quiénes somos?', 'FAQs', 'Contáctanos'],
+    },
+  ];
+
+  constructor() {}
+
+  toggleSidenav() {
+    this.sidenavHidden = !this.sidenavHidden;
+  }
+
+  toggleContent(routeTitle: string) {
+    if (this.activeRoute === routeTitle) {
+      this.showContent = !this.showContent;
+    } else {
+      this.activeRoute = routeTitle;
+      this.showContent = true;
+    }
+  }
+
+  isRouteActive(routeTitle: string): boolean {
+    return this.activeRoute === routeTitle;
+  }
+}
