14a3f2ffcda1ad49e6c10db092d763f52283c677
josuehoenicka <josuee.vzla@gmail.com>
2024-04-01 15:03:09 -0300

    feat: radio styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { Component} from '@angular/core';
 import { SidenavComponent } from './common/components/sidenav/sidenav.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { RadioComponent } from './radio/radio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    SidenavComponent
+    SidenavComponent,RadioComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   constructor() {}
-  currentView: any = "Dashboard";
+  currentView: string = "Radio" ;
 
-  onRouteChange(route: string) {
-    console.log(route);
+  onRouteChange(route: any) {
     this.currentView = route;
   }
 }
